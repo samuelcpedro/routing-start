4eadd23baf4d3f12eead03168db603fbc1c168ad
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-09 17:08:58 -0100

    Outsourcing the Route Configuration
    Outsourcing the Route Configuration
    Section 11, Lecture 135

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+      path: 'users', component: UsersComponent, children: [
+        { path: ':id/:name', component: UserComponent },
+      ]
+    },
+    {
+      path: 'servers', component: ServersComponent, children: [
+        { path: ':id', component: ServerComponent },
+        { path: ':id/edit', component: EditServerComponent },
+      ]
+    },
+    { path: 'not-found', component: PageNotFoundComponent },
+    { path: '**', redirectTo: '/not-found' }, // must be the last route
+  ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule] // of this modules that are being import wich of them  should i export
+})
+export class AppRoutingModule {
+}
